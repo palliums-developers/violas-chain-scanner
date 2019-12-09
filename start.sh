59c959bd6df403a52e708810c94f6acdaac41d25
Richard-Su <suliquangm@gmail.com>
2019-12-09 17:57:06 +0800

    modify start/stop scripy;

--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
-nohup python3.6 `pwd`/ExplorerCore.py > nohup.out 2>&1 &
+nohup python3.6 `pwd`/LibraExplorerCore.py > nohup.out 2>&1 &
+nohup python3.6 `pwd`/ViolasExplorerCore.py > nohup.out 2>&1 &
--- stop.sh
+++ stop.sh
@@ -1,1 +1,2 @@
-ps -ef | grep -v grep | grep explorer-core | awk '{print $2}' | xargs kill -9
+ps -ef | grep -v grep | grep LibraExplorerCore | awk '{print $2}' | xargs kill -9
+ps -ef | grep -v grep | grep ViolasExplorerCore | awk '{print $2}' | xargs kill -9
