5ac4189fe4097089e3f392f0953a9d585634280e
Richard-Su <suliquangm@gmail.com>
2019-12-24 18:43:54 +0800

    modify start script;

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-nohup python3.6 `pwd`/LibraExplorerCore.py > nohup.out 2>&1 &
-nohup python3.6 `pwd`/ViolasExplorerCore.py > nohup.out 2>&1 &
+nohup python3.6 `pwd`/LibraExplorerCore.py > LibraNohup.out 2>&1 &
+nohup python3.6 `pwd`/ViolasExplorerCore.py > ViolasNohup.out 2>&1 &
