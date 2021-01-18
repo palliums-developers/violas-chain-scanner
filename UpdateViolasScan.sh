2b9c4fd13fd2025ac912e08538d020dea3b0ce61
Richard-Su <suliquangm@gmail.com>
2021-01-18 17:20:48 +0800

    remove project update script;

--- UpdateViolasScan.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo docker stop violas-scan
-sudo docker rm violas-scan
-sudo docker image rm violas-scan
-sed -i "s/LibraExplorerCore/ViolasExplorerCore/g" Dockerfile
-sudo docker image build --no-cache -t violas-scan .
-sudo docker run --name=violas-scan --network=host -d violas-scan
